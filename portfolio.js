193e4e5e7a599bb57b36b4a14b1bc14d1ecb67fb
Rajat <rajat.cse.up@gmail.com>
2025-12-19 01:41:27 +0530

    files uploaded

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var Typed = new Typed(".text", {
+    strings: ["frontend Developer", "Graphics Designer", "Freelancer"],
+    typespeed: 100,
+    backspeed: 100,
+    backdelay: 1000,
+    loop: true
+})
